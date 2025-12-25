653d6fa23a12e655f11fd3e83e7c7087c55f06ed
FaridG7 <goharifarid7@gmail.com>
2025-12-25 18:18:51 +0330

    some changes to the lazyvim

---
+++ nvim/lua/plugins/extend-snacks-terminal.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          position = "right",
+          width = 0.3, -- This sets it to 30% of your screen width
+        },
+      },
+    },
+  },
+}
